81b918bb4151f0b4f50f58aa173dd751a1791cd6
nmke-de <github@nmke.de>
2021-12-20 17:01:34 +0100

    Using  now.

--- select.sh
+++ select.sh
@@ -6,5 +6,6 @@ get_data () {
 	sed '1,/^#EOF$/d' < "$SELF" | tar xz -O "$1"
 }
 
-get_data elb.tsv | awk "BEGIN{FS=\"\\t\"} $(D $(get_data elb.tsv | wc -l | cut -d' ' -f1))==NR{print \$1 \" \" \$4 \":\" \$5 \"\\t\" \$6}"
+lines=$(D $(get_data elb.tsv | wc -l | cut -d' ' -f1))
+get_data elb.tsv | awk "BEGIN{FS=\"\\t\"} $(D $lines)==NR{print \$1 \" \" \$4 \":\" \$5 \"\\t\" \$6}"
 
