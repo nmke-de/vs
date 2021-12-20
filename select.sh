7e42355cb241688e92910e4ee2f40708d92db70a
nmke-de <github@nmke.de>
2021-12-20 15:56:22 +0100

    Implemented get_data().

--- select.sh
+++ select.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # Change the above line if you need to.
 
+SELF="$0"
+get_data () {
+	sed '1,/^#EOF$/d' < "$SELF" | tar xz -O "$1"
+}
+
 line $(D $(wc -l elb.tsv | cut -d' ' -f1)) from elb.tsv | awk 'BEGIN{FS="\t"}{print $1 " " $4 ":" $5 "\t" $6}'
+
+#EOF
 
