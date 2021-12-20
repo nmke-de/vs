48757a2f8a1a69682f201240e92cb297c7289b47
nmke-de <github@nmke.de>
2021-12-20 16:03:20 +0100

    Added Makefile

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+vs: select.sh elb.tsv
+	cat select.sh > $@
+	tar cz elb.tsv > $@
+	chmod +x $@
+
--- select.sh
+++ select.sh
@@ -8,5 +8,6 @@ get_data () {
 
 line $(D $(wc -l elb.tsv | cut -d' ' -f1)) from elb.tsv | awk 'BEGIN{FS="\t"}{print $1 " " $4 ":" $5 "\t" $6}'
 
+exit 0
 #EOF
 
