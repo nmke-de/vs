53ae362fa6d478dbd818f3e3f2782178147c9e11
nmke-de <github@nmke.de>
2021-12-20 15:22:25 +0100

    Changed lines in select.sh to new bible source format.

--- select.sh
+++ select.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Change the above line if you need to.
 
-book="$(elb -l | cut -d'(' -f2 | tr -d ')' | line $(D 66))"
-echo $book
+#book="$(elb -l | cut -d'(' -f2 | tr -d ')' | line $(D 66))"
+#echo $book
 
+raw=`line $(D $(wc -l elb.tsv | cut -d' ' -f1)) from elb.tsv`
+echo $raw
