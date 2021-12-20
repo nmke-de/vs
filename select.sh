3af6721c6ee570dec69a15fd407288d070ae97af
nmke-de <github@nmke.de>
2021-12-20 16:54:57 +0100

    Collapsed two commands in select.sh

--- select.sh
+++ select.sh
@@ -6,5 +6,5 @@ get_data () {
 	sed '1,/^#EOF$/d' < "$SELF" | tar xz -O "$1"
 }
 
-get_data elb.tsv | awk "$(D $(get_data elb.tsv | wc -l | cut -d' ' -f1))==NR{print}" | awk 'BEGIN{FS="\t"}{print $1 " " $4 ":" $5 "\t" $6}'
+get_data elb.tsv | awk "BEGIN{FS=\"\\t\"} $(D $(get_data elb.tsv | wc -l | cut -d' ' -f1))==NR{print \$1 \" \" \$4 \":\" \$5 \"\\t\" \$6}"
 
