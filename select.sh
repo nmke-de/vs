d6e109caec34aed7f3e440ce030588a9051b5274
nmke-de <github@nmke.de>
2021-12-20 15:37:30 +0100

    Removed commented lines

--- select.sh
+++ select.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 # Change the above line if you need to.
 
-#book="$(elb -l | cut -d'(' -f2 | tr -d ')' | line $(D 66))"
-#echo $book
-
 line $(D $(wc -l elb.tsv | cut -d' ' -f1)) from elb.tsv | awk 'BEGIN{FS="\t"}{print $1 " " $4 ":" $5 "\t" $6}'
-#echo $raw
+
