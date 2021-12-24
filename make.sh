9a8f6c1adbfcff86968ee0370e70e1b19eb1b43f
nmke-de <github@nmke.de>
2021-12-24 14:04:54 +0100

    Filled make.sh with actual source code, not copycat.

--- make.sh
+++ make.sh
@@ -6,6 +6,23 @@ get_data () {
 	sed '1,/^#EOF$/d' < "$SELF" | tar xz -O "$1"
 }
 
-lines=$(D $(get_data elb.tsv | wc -l | cut -d' ' -f1))
-get_data elb.tsv | awk "BEGIN{FS=\"\\t\"; r=int(rand()*$lines)+1} r==NR{print \$1 \" \" \$4 \":\" \$5 \"\\t\" \$6}"
+infile=""
+outfile=""
+ap_state="normal"
+while [ $# -gt 0 ]; do
+	[ $ap_state = "get_outfile" ] && outfile=$1 && ap_state="normal" && continue
+	[ $1 = "-o" ] && [$ap_state = "normal" ] && ap_state="get_outfile" && continue
+	[ $1 = "-h" ] && echo "Syntax: $0 [-h|-o Outputfile] Inputfile" && exit 0
+	[ $1 = "--help" ] && echo "Syntax: $0 [--help|-h|-o Outputfile] Inputfile" && exit 0
+done
+
+test -z $infile && echo "No input file." >> /dev/stderr && exit 1
+
+echo "Using $infile to make Verse/Selector $outfile..." >> /dev/stderr
+get_data select.sh > $outfile
+echo 'exit 0' >> $outfile
+echo '#EOF' >> $outfile
+tar cz $infile >> $outfile
+chmod +x $outfile
+echo "Done."
 
