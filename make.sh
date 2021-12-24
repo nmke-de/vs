543cef6f3eb66f8bc1b479eaa7ec13cdc2dfae52
nmke-de <github@nmke.de>
2021-12-24 14:43:28 +0100

    Hotfixed syntax in make.sh and fixed rng in select.sh

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ ap_state="normal"
 while [ $# -gt 0 ]; do
 	echo $1
 	[ $ap_state = "get_outfile" ] && outfile=$1 && ap_state="normal"
-	[ $1 = "-o" ] && [$ap_state = "normal" ] && ap_state="get_outfile"
+	[ $1 = "-o" ] && [ $ap_state = "normal" ] && ap_state="get_outfile"
 	[ $1 = "-h" ] && echo "Syntax: $0 [--help|-h|-o Outputfile] Inputfile" && exit 0
 	[ $1 = "--help" ] && echo "Syntax: $0 [--help|-h|-o Outputfile] Inputfile" && exit 0
 	[ $1 != "-o" ]  && [ $ap_state = "normal" ] && infile=$1
